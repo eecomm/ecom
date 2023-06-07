73ddf7d47e3f27d47ab0e29705e0a0f6fb91cb14
advait-shetye <shetyeadvait34@gmail.com>
2023-06-07 21:31:40 +0530

    Navbar first cut

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import Slider from '../components/slider/Slider'
-
+import Navbar from '../components/navbar/navbar'
 export default function Home() {
   return (
     <>
-      {/* <Slider></Slider> */}
+      <Navbar></Navbar>
     </>
   )
 }
